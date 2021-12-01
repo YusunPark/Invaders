4c76dd92af5a0ba203327ea97402f8959d49e28b
Yusun <solarpy@hanyang.ac.kr>
2021-12-01 23:44:34 +0900

    feat : Change background img and game bgm
    INVADER-104

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -165,7 +165,7 @@ public final class DrawManager {
 		backBufferGraphics
 				.fillRect(0, 0, screen.getWidth(), screen.getHeight());
 		
-		Image img = new ImageIcon("res/moon.png").getImage();
+		Image img = new ImageIcon("res/img.jpg").getImage();
 		backBufferGraphics.drawImage(img, 0, 0, null);
 
 		fontRegularMetrics = backBufferGraphics.getFontMetrics(fontRegular);
@@ -311,7 +311,7 @@ public final class DrawManager {
 		String instructionsString =
 				"select with w+s / arrows, confirm with space";
 
-		backBufferGraphics.setColor(Color.GRAY);
+		backBufferGraphics.setColor(Color.WHITE);
 		drawCenteredRegularString(screen, instructionsString,
 				screen.getHeight() / 2);
 
@@ -544,7 +544,7 @@ public final class DrawManager {
 		if (acceptsInput)
 			backBufferGraphics.setColor(Color.GREEN);
 		else
-			backBufferGraphics.setColor(Color.GRAY);
+			backBufferGraphics.setColor(Color.WHITE);
 		drawCenteredRegularString(screen, continueOrExitString,
 				screen.getHeight() / 2 + fontRegularMetrics.getHeight() * 10);
 	}
@@ -562,7 +562,7 @@ public final class DrawManager {
 		backBufferGraphics.setColor(Color.GREEN);
 		drawCenteredBigString(screen, highScoreString, screen.getHeight() / 8);
 
-		backBufferGraphics.setColor(Color.GRAY);
+		backBufferGraphics.setColor(Color.WHITE);
 		drawCenteredRegularString(screen, instructionsString,
 				screen.getHeight() / 5);
 	}
@@ -608,7 +608,7 @@ public final class DrawManager {
 		drawCenteredBigString(screen, HelpString,
 				screen.getHeight() / 8);
 
-		backBufferGraphics.setColor(Color.GRAY);
+		backBufferGraphics.setColor(Color.WHITE);
 		drawCenteredRegularString(screen, instructionsString,
 				screen.getHeight() / 5);
 
